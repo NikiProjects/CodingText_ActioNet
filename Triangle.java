aa620781a9ffb6358f7dcea7d488fe15db92f025
NikiProjects <NikitaK01@gmail.com>
2016-07-27 21:35:48 -0400

    Create Triangle.java

---
+++ Triangle.java
@@ -0,0 +1,53 @@
+package Task1;
+
+public class Triangle implements Shape {
+
+private double base1;
+private double height;
+private double base2;
+
+public Triangle(double base1, double height, double base2)
+{
+	this.base1 = base1;
+	this.height = height;
+	this.base2 = base2;
+}
+
+
+
+public double getArea()
+{
+	double calc1 = base1 * height;
+	double area_Triangle = calc1 / 2;
+	
+	return area_Triangle;
+}
+
+
+public double getPerimeter()
+{
+	double perimeter_Triangle = base1 + base2 + height;
+	
+	return perimeter_Triangle;
+}
+
+	
+
+
+//	public static void main(String[] args)
+//	{
+		// TODO Auto-generated method stub
+
+//	}
+
+
+
+
+
+
+
+
+
+
+
+}
